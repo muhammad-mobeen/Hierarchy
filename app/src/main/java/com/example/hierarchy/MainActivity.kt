db1ec63b89732d031d5d54650be9c92fca6279f5
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2023-06-01 17:07:01 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/hierarchy/MainActivity.kt
+++ app/src/main/java/com/example/hierarchy/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.hierarchy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    open class Vehicle(Brand:String, Color:String) {
+        var brand: String = Brand
+        var color: String = Color
+        open fun drive(){
+            println("Driving the vehicle!!")
+        }
+    }
+
+    //derived class
+    class Car(Brand: String, Color: String, noDoors: Int): Vehicle(Brand, Color) {
+        var numberofDoors: Int = noDoors
+        override fun drive() {
+            println("Driving the car with $numberofDoors doors")
+        }
+    }
+
+    //derived class
+    class Motorcycle(Brand: String, Color: String, HasSidecar: Boolean): Vehicle(Brand, Color) {
+        var hasSidecar: Boolean = HasSidecar
+        override fun drive() {
+            println("Has Sidecar: $hasSidecar")
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var car = Car("Honda", "Black", 4)
+        println("----------------------------------------------------------------------------")
+        car.drive()
+//        Toast.makeText(this,,Toast.LENGTH_LONG).show()
+        var motorcycle = Motorcycle("Honda", "Black", HasSidecar = true)
+        println("----------------------------------------------------------------------------")
+        motorcycle.drive()
     }
+
+
+
 }
